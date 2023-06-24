9306f96ce889434a98f670589b6f768363649674
최수용 <dpfprtus@gmail.com>
2023-06-24 17:59:02 +0900

    Revert "[FEAT] API 기능 개발 완료"

--- BE/src/api/landing/landing.index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const landingController = require("./landing.controller");
-
-router.post("/survey", landingController.createInfo);
-
-module.exports = router;
