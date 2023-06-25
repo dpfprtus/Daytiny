07707a95c27d2f4c75779869cdd43c6538869073
dpfprtus <dpfprtus@gmail.com>
2023-06-25 17:11:39 +0900

    [FEAT] CORS

--- BE/server.js
+++ BE/server.js
@@ -1,11 +1,14 @@
 const express = require("express"); //express를 설치했기 때문에 가져올 수 있다.
 const logger = require("morgan");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
