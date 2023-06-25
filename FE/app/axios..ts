4de118a1186029b85cadec4d9cf4b704abbbd75f
unknown <rlaghdtlr012@naver.com>
2023-06-25 22:08:23 +0900

    [FEAT] 핸드폰 번호 유효성 검사 정규식 추가 및 axios URL 설정완료

--- FE/app/axios..ts
+++ FE/app/axios..ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const axiosBaseURL = axios.create({
-  baseURL: process.env.HOST,
-  headers: {
-    'Content-Type': 'application/json'
-  },
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  //headers: {
+  //  'Content-Type': 'application/json'
+  //},
 });
 
 export default axiosBaseURL;
--- FE/next.config.js
+++ FE/next.config.js
@@ -1,7 +1,14 @@
+require('dotenv').config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+};
